42faab64c474f2a2c7ff016a14d5d705a822f7ac
尹中楠 <yinzhongnan@baidu.com>
2023-10-13 19:49:28 +0800

    add modules: rpc-core,demo-server,demo-client

---
+++ demo-server/src/main/java/com/zyan/forward/Main.java
@@ -0,0 +1,13 @@
+package com.zyan.forward;
+
+/**
+ * desc
+ *
+ * @author yinzhongnan
+ * @date 2023/10/13 7:48 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ rpc-core/src/main/java/com/zyan/forward/Main.java
@@ -0,0 +1,13 @@
+package com.zyan.forward;
+
+/**
+ * desc
+ *
+ * @author yinzhongnan
+ * @date 2023/10/13 7:47 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
